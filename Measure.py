75652b01b1a5f67aad928d9aa8777edde469bf1b
CaioLuizHermann <kamijogaroblox@gmail.com>
2026-03-17 09:10:02 -0300

    Corrected bugs and removed "go back" option

--- Measure.py
+++ Measure.py
@@ -3,44 +3,42 @@ while end == False:
     first_value = 0
     value = 0
     result = 0
-    c1 = int(input("Choose the first measure system \n <1> Cm \n <2> M \n <3> Km \n <4> End Program"))
+    c1 = int(input("Choose the first measure system \n <1> Cm \n <2> M \n <3> Km \n <4> End Program\n"))
     if c1 == 1:
-        m1 = int(input("Insert the quantity: \n"))
+        m1 = float(input("Insert the quantity: \n"))
         first_value = 1
     elif c1 == 2:
-        m1 = int(input("Insert the quantity: \n"))
+        m1 = float(input("Insert the quantity: \n"))
         first_value = 2
     elif c1 == 3:
-        m1 = int(input("Insert the quantity: \n"))
+        m1 = float(input("Insert the quantity: \n"))
         first_value = 3
     elif c1 == 4:
         end = True
     elif c1 >= 5 or c1 <= 0:
-        print("Error 0: \n Invalid number")
-    c2 = int(input("Choose the second measure system \n <1> Cm \n <2> M \n <3> Km \n <4> Go Back"))
-        if c2 == 1:
-            if first_value == 1:
-                result = m1
-            if first_value == 2:
-                result = m1/100
-            if first_value == 3:
-                result = m1/100000
-        elif c2 == 2:
-            if first_value == 1:
-                result = m1*100
-            if first_value == 2:
-                result = m1
-            if first_value == 3:
-                result = m1/1000
-        elif c2 == 3:
-            if first_value == 1:
-                result = m1*100000
-            if first_value == 2:
-                result = m1*1000
-            if first_value == 3:
-                result = m1
-        elif c2 == 4:
-            end = True
-        elif c2 >= 5 or c2 <= 0:
-            print("Error 0: \n Invalid number")
-    print("The result is... \n ", result )
+        print("Error 0: \n Invalid number\n")
+    c2 = int(input("Choose the second measure system \n <1> Cm \n <2> M \n <3> Km \n "))
+    if c2 == 1:
+        if first_value == 1:
+            result = m1
+        if first_value == 2:
+            result = m1/100
+        if first_value == 3:
+            result = m1/100000
+    elif c2 == 2:
+        if first_value == 1:
+            result = m1*100
+        if first_value == 2:
+            result = m1
+        if first_value == 3:
+            result = m1/1000
+    elif c2 == 3:
+        if first_value == 1:
+            result = m1/100000
+        if first_value == 2:
+            result = m1/1000
+        if first_value == 3:
+            result = m1
+    elif c2 >= 4 or c2 <= 0:
+       print("Error 0: \n Invalid number")
+        print("The result is... \n ", result )
