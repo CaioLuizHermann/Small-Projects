8cae558dbdbfc01a91bc11d53922be88aabc61e4
Caio Luiz Hermann <kamijogaroblox@gmail.com>
2026-03-23 09:33:06 -0300

    Added Insert command

--- Measure System Interface/main.py
+++ Measure System Interface/main.py
@@ -3,16 +3,38 @@ import convert
 window = tk.Tk()
 window.title("Conversor de Unidades")
 window.geometry("1100x900")
-
-text = tk.Label(window, text="\nWelcome!\n Select one of the units bellow using only numbers: \n <1> Cm \n <2> M \n <3> Km \n", font=("Arial", 20))
-text.pack()
+def clean_entry_unit():
+    if entry_unit.get() == "Number":
+        entry_unit.delete(0, tk.END)
+        entry_unit.config(fg="black")
+def reset_entry_unit():
+    if entry_unit.get() == "":
+        entry_unit.insert(0, "Number")
+        entry_unit.config(fg="gray")
+def clean_entry_value():
+    if entry_value.get() == "Quantity":
+        entry_value.delete(0, tk.END)
+        entry_value.config(fg="black")
+def reset_entry_value():
+    if entry_value.get() == "":
+        entry_value.insert(0, "Quantity")
+        entry_value.config(fg="gray")
+text1 = tk.Label(window, text="\nWelcome!\n Select one of the units bellow using only numbers: \n <1> Cm \n <2> M \n <3> Km \n", font=("Arial", 20))
+text1.pack()
 
 entry_unit = tk.Entry(window, width=10, font=("Arial", 14), bg="lightgray")
+entry_unit.insert(0, "Number")
+entry_unit.config(fg="gray")
 entry_unit.pack()
+entry_unit.bind("<FocusIn>", clean_entry_unit)
+entry_unit.bind("<FocusOut>", reset_entry_unit)
 entry_value = tk.Entry(window,width=10, font=("Arial", 14), bg="lightgray")
+entry_value.insert(0, "Quantity")
 text_value = tk.Label(window, text="\n Now, in the box bellow, insert the quantity you want to convert \n", font=("Arial", 20))
 text_value.pack()
 entry_value.pack()
+entry_value.bind("<FocusIn>", clean_entry_value)
+entry_value.bind("<FocusOut>", reset_entry_value)
 entry_final_unit = tk.Entry(window, width=10, font=("Arial", 14), bg="lightgray")
 text_final_unit = tk.Label(window, text="\n Finally, insert the number of the unit you want to convert to using the numbers bellow: \n <1> Cm \n <2> M \n <3> Km \n", font=("Arial", 20))
 text_final_unit.pack()
