db8e0d789ea73713c0868eed035c1ba4c28db431
CaioLuizHermann <kamijogaroblox@gmail.com>
2026-03-25 09:03:53 -0300

    Finished Insert Command and created new functions that previes what you're suposed to write

--- Measure System Interface/main.py
+++ Measure System Interface/main.py
@@ -3,39 +3,53 @@ import convert
 window = tk.Tk()
 window.title("Conversor de Unidades")
 window.geometry("1100x900")
-def clean_entry_unit():
-    if entry_unit.get() == "Number":
+def clean_entry_unit(event):
+    if entry_unit.get() == "First Unit, Ex: 1":
         entry_unit.delete(0, tk.END)
         entry_unit.config(fg="black")
-def reset_entry_unit():
+def reset_entry_unit(event):
     if entry_unit.get() == "":
-        entry_unit.insert(0, "Number")
+        entry_unit.insert(0, "First Unit, Ex: 1")
         entry_unit.config(fg="gray")
-def clean_entry_value():
-    if entry_value.get() == "Quantity":
+def clean_entry_value(event):
+    if entry_value.get() == "Quantity, Ex: 100":
         entry_value.delete(0, tk.END)
         entry_value.config(fg="black")
-def reset_entry_value():
+def reset_entry_value(event):
     if entry_value.get() == "":
-        entry_value.insert(0, "Quantity")
+        entry_value.insert(0, "Quantity, Ex: 100")
         entry_value.config(fg="gray")
+def clean_entry_final_unit(event):
+    if entry_final_unit.get() == "Final Unit, Ex: 2":
+        entry_final_unit.delete(0, tk.END)
+        entry_final_unit.config(fg="black")
+def reset_entry_final_unit(event):
+    if entry_final_unit.get() == "":
+        entry_final_unit.insert(0, "Final Unit, Ex: 2")
+        entry_final_unit.config(fg="gray")
+
 text1 = tk.Label(window, text="\nWelcome!\n Select one of the units bellow using only numbers: \n <1> Cm \n <2> M \n <3> Km \n", font=("Arial", 20))
 text1.pack()
 
-entry_unit = tk.Entry(window, width=10, font=("Arial", 14), bg="lightgray")
-entry_unit.insert(0, "Number")
+entry_unit = tk.Entry(window, width=40, font=("Arial", 16), bg="lightgray")
+entry_unit.insert(0, "First Unit, Ex: 1")
 entry_unit.config(fg="gray")
 entry_unit.pack()
 entry_unit.bind("<FocusIn>", clean_entry_unit)
 entry_unit.bind("<FocusOut>", reset_entry_unit)
-entry_value = tk.Entry(window,width=10, font=("Arial", 14), bg="lightgray")
-entry_value.insert(0, "Quantity")
+entry_value = tk.Entry(window,width=40, font=("Arial", 16), bg="lightgray")
+entry_value.insert(0, "Quantity, Ex: 100")
+entry_value.config(fg="gray")
 text_value = tk.Label(window, text="\n Now, in the box bellow, insert the quantity you want to convert \n", font=("Arial", 20))
 text_value.pack()
 entry_value.pack()
 entry_value.bind("<FocusIn>", clean_entry_value)
 entry_value.bind("<FocusOut>", reset_entry_value)
-entry_final_unit = tk.Entry(window, width=10, font=("Arial", 14), bg="lightgray")
+entry_final_unit = tk.Entry(window, width=40, font=("Arial", 16), bg="lightgray")
+entry_final_unit.insert(0, "Final Unit, Ex: 2")
+entry_final_unit.config(fg= "gray")
+entry_final_unit.bind("<FocusIn>", clean_entry_final_unit)
+entry_final_unit.bind("<FocusOut>", reset_entry_final_unit)
 text_final_unit = tk.Label(window, text="\n Finally, insert the number of the unit you want to convert to using the numbers bellow: \n <1> Cm \n <2> M \n <3> Km \n", font=("Arial", 20))
 text_final_unit.pack()
 entry_final_unit.pack()
@@ -61,7 +75,7 @@ def calculate():
         error = tk.Label(text="Error", font=("Arial", 14))
         error.pack()
 
-calculate_button = tk.Button(window, text="convert", command=calculate, width=10, height=1, font=("Arial", 20), bg="lightgray")
+calculate_button = tk.Button(window, text="convert", command=calculate, width=50, height=1, font=("Arial", 20), bg="lightgray")
 calculate_button.pack()
 text_result.pack()
 window.mainloop()
